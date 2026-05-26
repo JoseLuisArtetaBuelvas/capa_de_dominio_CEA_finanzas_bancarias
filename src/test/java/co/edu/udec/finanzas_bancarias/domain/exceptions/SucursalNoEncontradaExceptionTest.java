8791dfd46a1864ff0b91b6c912de8c5d1bf3dc86
José Luis Arteta Buelvas <jartetab@unicartagena.edu.co>
2026-05-26 15:35:03 -0500

    test(domain): crea test de SucursalNoEncontradaException

---
+++ src/test/java/co/edu/udec/finanzas_bancarias/domain/exceptions/SucursalNoEncontradaExceptionTest.java
@@ -0,0 +1,26 @@
+package co.edu.udec.finanzas_bancarias.domain.exceptions;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class SucursalNoEncontradaExceptionTest {
+
+    @Test
+    void debeCrearExepcionConMensaje() {
+        var excepcion = new SucursalNoEncontradaException("Sucursal no encontrada");
+        assertThat(excepcion.getMessage()).isEqualTo("Sucursal no encontrada");
+    }
+
+    @Test
+    void debeSerInstanciaDeRuntimeException() {
+        var excepcion = new SucursalNoEncontradaException("error");
+        assertThat(excepcion).isInstanceOf(SucursalNoEncontradaException.class);
+    }
+
+    @Test
+    void debeRealizarseEnContexto(){
+        assertThatThrownBy(() -> {
+            throw new SucursalNoEncontradaException("error");
+        }).isInstanceOf(SucursalNoEncontradaException.class).hasMessageContaining("error");
+    }
+}
