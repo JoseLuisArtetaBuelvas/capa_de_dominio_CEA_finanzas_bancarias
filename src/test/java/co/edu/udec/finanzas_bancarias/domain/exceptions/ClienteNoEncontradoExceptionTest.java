e600eb504cff71d0e06b816c7ead1f4090f346c9
José Luis Arteta Buelvas <jartetab@unicartagena.edu.co>
2026-05-26 15:02:03 -0500

    test(domain): crea test de ClineteNoEncontradoException

---
+++ src/test/java/co/edu/udec/finanzas_bancarias/domain/exceptions/ClienteNoEncontradoExceptionTest.java
@@ -0,0 +1,26 @@
+package co.edu.udec.finanzas_bancarias.domain.exceptions;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ClienteNoEncontradoExceptionTest {
+    @Test
+    public void debeCrearExcepcionConMensaje() {
+        var excepcion = new ClienteNoEncontradoException("Cliente no encontrado");
+        assertThat(excepcion.getMessage()).isEqualTo("Cliente no encontrado");
+    }
+
+    @Test
+    public void debeSerInstanciaDeRuntimeException() {
+        var excepcion = new ClienteNoEncontradoException("error");
+        assertThat(excepcion).isInstanceOf(ClienteNoEncontradoException.class);
+    }
+
+    @Test
+    public void debeLanzarseEnContextoCorrecto(){
+        assertThatThrownBy(() -> {
+            throw new ClienteNoEncontradoException("error");
+        }).isInstanceOf(ClienteNoEncontradoException.class).hasMessage("error");
+    }
+}
