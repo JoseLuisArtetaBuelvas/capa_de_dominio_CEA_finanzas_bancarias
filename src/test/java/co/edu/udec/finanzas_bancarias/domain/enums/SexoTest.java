0bc92ef3f40ded50cec6333b92cbdb16230fa0ed
José Luis Arteta Buelvas <jartetab@unicartagena.edu.co>
2026-05-25 19:37:01 -0500

    test(domain): agrega test del tipo de Sexo

---
+++ src/test/java/co/edu/udec/finanzas_bancarias/domain/enums/SexoTest.java
@@ -0,0 +1,22 @@
+package co.edu.udec.finanzas_bancarias.domain.enums;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class SexoTest {
+
+    @Test
+    public void esFemenino(){
+        var sexo = Sexo.FEMININO;
+        assertThat(sexo).isEqualTo(Sexo.FEMININO);
+    }
+
+    public void esMasculino(){
+        var sexo = Sexo.MASCULINO;
+        assertThat(sexo).isEqualTo(Sexo.MASCULINO);
+    }
+
+    public void debeFallarConValorInexistente(){
+        assertThatThrownBy(()->TipoCuenta.valueOf(null)).isInstanceOf(IllegalArgumentException.class);
+    }
+}
