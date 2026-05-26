e611868a1df2151bfc8f89564d3eb3d5cb2d48ee
José Luis Arteta Buelvas <jartetab@unicartagena.edu.co>
2026-05-25 21:34:25 -0500

    test(domain): agrega test de SaldoInsuficienteException

---
+++ src/test/java/co/edu/udec/finanzas_bancarias/domain/exceptions/SaldoInsuficienteExceptionTest.java
@@ -0,0 +1,26 @@
+package co.edu.udec.finanzas_bancarias.domain.exceptions;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class SaldoInsuficienteExceptionTest {
+
+    @Test
+    public void debeCrearExcepcionConMensaje() {
+        var excepcion = new SaldoInsuficienteException("Saldo insuficiente para realizar el retiro");
+        assertThat(excepcion.getMessage()).isEqualTo("Saldo insuficiente para realizar el retiro");
+    }
+
+    @Test
+    public void debeSerInstanciaDeRuntimeException(){
+        var excepcion = new SaldoInsuficienteException("error");
+        assertThat(excepcion).isInstanceOf(SaldoInsuficienteException.class);
+    }
+
+    @Test
+    public void debeLanzarseEnCointextoCorrecto(){
+        assertThatThrownBy(()->{
+            throw new SaldoInsuficienteException("error");
+        }).isInstanceOf(SaldoInsuficienteException.class).hasMessageContaining("error");
+    }
+}
