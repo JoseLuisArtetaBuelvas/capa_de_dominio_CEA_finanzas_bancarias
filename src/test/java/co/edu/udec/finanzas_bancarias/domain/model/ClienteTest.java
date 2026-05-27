f2db0239f4d3410a9012dee1b8084d692ac311f7
José Luis Arteta Buelvas <jartetab@unicartagena.edu.co>
2026-05-27 00:05:50 -0500

    test(domain): agrega test de Cliente

---
+++ src/test/java/co/edu/udec/finanzas_bancarias/domain/model/ClienteTest.java
@@ -0,0 +1,32 @@
+package co.edu.udec.finanzas_bancarias.domain.model;
+import co.edu.udec.finanzas_bancarias.domain.valueobjects.CodigoCliente;
+import co.edu.udec.finanzas_bancarias.domain.valueobjects.Direccion;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ClienteTest {
+
+    @Test
+    public void debeCrearClienteValido(){
+        var cliente = new Cliente(new CodigoCliente("11935842254"), "José Luis Arteta Buelvas", new Direccion("Calle 123 # 45-67"));
+        assertThat(cliente.getCodigoCliente().value()).isEqualTo("11935842254");
+        assertThat(cliente.getNombre()).isEqualTo("José Luis Arteta Buelvas");
+        assertThat(cliente.getDireccion().value()).isEqualTo("Calle 123 # 45-67");
+    }
+
+    @Test
+    public void debeFallarConCodigoClienteNulo(){
+        assertThatThrownBy(()-> new Cliente(null, "José Luis Arteta Buelvas", new Direccion("Calle 123 # 45-67"))).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("nulo");
+    }
+
+    @Test
+    public void debeFallarConNombreNulo(){
+        assertThatThrownBy(()-> new Cliente(new CodigoCliente("11935842254"), null, new Direccion("Calle 123 # 45-67"))).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("nulo");
+    }
+
+    @Test
+    public void debeFallarConDireccionNula(){
+        assertThatThrownBy(()-> new Cliente(new CodigoCliente("11935842254"), "José Luis Arteta Buelvas", null)).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("nula");
+    }
+}
