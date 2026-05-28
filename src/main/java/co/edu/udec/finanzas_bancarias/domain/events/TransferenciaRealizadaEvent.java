6051d44d31403503b827dc80a976f75b50849f23
José Luis Arteta Buelvas <jartetab@unicartagena.edu.co>
2026-05-27 21:36:17 -0500

    feat(domain): crea TransferenciaRealizadaEvent como event

--- src/main/java/co/edu/udec/finanzas_bancarias/domain/events/TransferenciaRealizadaEvent.java
+++ src/main/java/co/edu/udec/finanzas_bancarias/domain/events/TransferenciaRealizadaEvent.java
@@ -1,5 +1,5 @@
 package co.edu.udec.finanzas_bancarias.domain.events;
 import java.time.Instant;
 
-public record TransferenciaRealizadaEvent(String cccOrigenm, String cccDestino, Instant fecha) {
+public record TransferenciaRealizadaEvent(String cccOrigen, String cccDestino, Instant fecha) {
 }
