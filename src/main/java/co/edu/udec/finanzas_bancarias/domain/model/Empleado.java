9098eedacf79c5d20d413d2347f41fa86e4f775c
José Luis Arteta Buelvas <jartetab@unicartagena.edu.co>
2026-05-27 19:12:16 -0500

    feat(domain): crear Empleado como model

--- src/main/java/co/edu/udec/finanzas_bancarias/domain/model/Empleado.java
+++ src/main/java/co/edu/udec/finanzas_bancarias/domain/model/Empleado.java
@@ -4,7 +4,6 @@ import co.edu.udec.finanzas_bancarias.domain.enums.Sexo;
 import co.edu.udec.finanzas_bancarias.domain.valueobjects.CodigoCliente;
 import co.edu.udec.finanzas_bancarias.domain.valueobjects.Direccion;
 import co.edu.udec.finanzas_bancarias.domain.valueobjects.Dni;
-import co.edu.udec.finanzas_bancarias.domain.valueobjects.NumeroSucursal;
 
 import java.time.LocalDate;
 
@@ -12,9 +11,9 @@ public class Empleado extends Cliente {
     private final Dni dni;
     private LocalDate fechaNacimiento;
     private Sexo sexo;
-    private final NumeroSucursal idSucursal;
+    private Sucursal sucursal;
 
-    public Empleado(CodigoCliente codigoCliente, String nombre, Direccion direccion, Dni dni, LocalDate fechaNacimiento, Sexo sexo, NumeroSucursal idSucursal) {
+    public Empleado(CodigoCliente codigoCliente, String nombre, Direccion direccion, Dni dni, LocalDate fechaNacimiento, Sexo sexo, Sucursal sucursal) {
         super(codigoCliente, nombre, direccion);
         if(dni == null){
             throw new IllegalArgumentException("El dni no puede ser nulo");
@@ -22,13 +21,14 @@ public class Empleado extends Cliente {
             throw new IllegalArgumentException("El fecha nacimiento no puede ser nula");
         }else if(sexo == null){
             throw new IllegalArgumentException("El sexo no puede ser nulo");
-        }else if(idSucursal == null){
-            throw new IllegalArgumentException("El Identificacion no puede ser nulo");
+        }else if(sucursal == null){
+            throw new IllegalArgumentException("El sucursal no puede ser nulo");
         }
+
         this.dni = dni;
         this.fechaNacimiento = fechaNacimiento;
         this.sexo = sexo;
-        this.idSucursal = idSucursal;
+        this.sucursal = sucursal;
     }
 
     public void actualizarFechaNacimiento(LocalDate fechaNacimiento) {
@@ -36,6 +36,13 @@ public class Empleado extends Cliente {
             throw new IllegalArgumentException("El fecha nacimiento no puede ser nula");
         }
         this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public void actualizarSucursal(Sucursal sucursal) {
+        if(sucursal == null){
+            throw new IllegalArgumentException("La sucursal no puede ser nula");
+        }
+        this.sucursal = sucursal;
     }
 
     public void actualizarSexo(Sexo sexo) {
@@ -48,5 +55,5 @@ public class Empleado extends Cliente {
     public Dni getDni() {return dni;}
     public LocalDate getFechaNacimiento() {return fechaNacimiento;}
     public Sexo getSexo() {return sexo;}
-    public NumeroSucursal getIdSucursal() {return idSucursal;}
+    public Sucursal getSucursal() {return sucursal;}
 }
