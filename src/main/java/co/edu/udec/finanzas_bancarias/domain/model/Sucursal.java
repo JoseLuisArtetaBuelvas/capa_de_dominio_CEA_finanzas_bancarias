bc37dbf88bfc9bade2019b81ba1fb60d368b923c
José Luis Arteta Buelvas <jartetab@unicartagena.edu.co>
2026-05-27 19:57:42 -0500

    feat(domain): crea Sucursal como model

--- src/main/java/co/edu/udec/finanzas_bancarias/domain/model/Sucursal.java
+++ src/main/java/co/edu/udec/finanzas_bancarias/domain/model/Sucursal.java
@@ -8,19 +8,22 @@ public class Sucursal {
     private NumeroSucursal numeroSucursal;
     private Direccion direccion;
     private String ciudadOperando;
-
-    public Sucursal(Integer idSucursal, NumeroSucursal numeroSucursal, Direccion direccion, String ciudadOperando) {
+    private Banco banco;
+    public Sucursal(Integer idSucursal, NumeroSucursal numeroSucursal, Direccion direccion, String ciudadOperando, Banco banco) {
         if(numeroSucursal == null){
             throw new IllegalArgumentException("El numero de sucursal no puede ser nulo");
         }else if(direccion == null){
             throw new IllegalArgumentException("La direccion no puede ser nula");
         }else if(ciudadOperando == null){
             throw new IllegalArgumentException("La ciudad donde opera la sucursal no puede ser nula");
+        }else if(banco == null){
+            throw new IllegalArgumentException("El banco no puede ser nula");
         }
         this.idSucursal = idSucursal;
         this.numeroSucursal = numeroSucursal;
         this.direccion = direccion;
         this.ciudadOperando = ciudadOperando;
+        this.banco = banco;
     }
 
     public void actualizarDireccion(Direccion direccion) {
@@ -44,8 +47,16 @@ public class Sucursal {
         this.ciudadOperando = ciudadOperando;
     }
 
+    public void actualizarBanco(Banco banco) {
+        if(banco == null){
+            throw new IllegalArgumentException("La banco no puede ser nulo");
+        }
+        this.banco = banco;
+    }
+
     public Integer getIdSucursal() {return this.idSucursal;}
     public NumeroSucursal getNumeroSucursal() {return this.numeroSucursal;}
     public Direccion getDireccion() {return this.direccion;}
     public String getCiudadOperando() {return this.ciudadOperando;}
+    public Banco getBanco() {return this.banco;}
 }
